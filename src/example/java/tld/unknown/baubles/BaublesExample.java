6d66995639621e5f9dd2923446bc3243456a9d25
Tom Tanticle <113810920+Tanticle@users.noreply.github.com>
2024-06-17 00:57:26 +0200

    Restructure:tm:

--- src/example/java/tld/unknown/baubles/BaublesExample.java
+++ src/example/java/tld/unknown/baubles/BaublesExample.java
@@ -1,7 +1,6 @@
-package tld.unknown.baubles_example;
+package tld.unknown.baubles;
 
 import net.minecraft.core.registries.BuiltInRegistries;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.Item;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.IEventBus;
@@ -12,28 +11,28 @@ import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
 import net.neoforged.neoforge.registries.DeferredHolder;
 import net.neoforged.neoforge.registries.DeferredRegister;
 import tld.unknown.baubles.api.BaublesAPI;
+import tld.unknown.baubles.api.BaublesData;
 
-@Mod(BaublesExample.MOD_ID)
+@Mod(BaublesData.MOD_ID)
 public class BaublesExample {
 
-    public static final String MOD_ID = "baubles_example";
-
-    private static final DeferredRegister<Item> REGISTRY_ITEMS = DeferredRegister.create(BuiltInRegistries.ITEM, MOD_ID);
+    private static final DeferredRegister<Item> REGISTRY_ITEMS = DeferredRegister.create(BuiltInRegistries.ITEM, BaublesData.MOD_ID);
 
     public static final DeferredHolder<Item, TestRingItem> ITEM_RING = REGISTRY_ITEMS.register(
             "ring",
             TestRingItem::new);
 
     public BaublesExample(IEventBus modEventBus) {
+        System.out.println("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
         REGISTRY_ITEMS.register(modEventBus);
     }
 
-    @EventBusSubscriber(modid = MOD_ID, bus = EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    @EventBusSubscriber(modid = BaublesData.MOD_ID, bus = EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
     public static final class ModBusSubscriber {
 
         @SubscribeEvent
         public static void onClientInit(final FMLClientSetupEvent event) {
-            BaublesAPI.getRenderers().registerRenderer(ResourceLocation.fromNamespaceAndPath(MOD_ID, "ring"), new TestRingItem.TestRenderer());
+            BaublesAPI.getRenderers().registerRenderer(BaublesData.id("ring"), new TestRingItem.TestRenderer());
         }
     }
 }
--- src/example/java/tld/unknown/baubles/TestRingItem.java
+++ src/example/java/tld/unknown/baubles/TestRingItem.java
@@ -1,4 +1,4 @@
-package tld.unknown.baubles_example;
+package tld.unknown.baubles;
 
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.VertexConsumer;
